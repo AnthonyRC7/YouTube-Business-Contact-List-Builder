3f621f05ad7dc00a16843539e0e1109e87186bc8
Anthony Remichris <119957123+AnthonyRC7@users.noreply.github.com>
2025-09-26 09:16:31 +0100

    Create text_proc.py

---
+++ text_proc.py
@@ -0,0 +1,22 @@
+import re
+from tqdm import tqdm
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+def preprocess_text(text_data):
+    preprocessed_text = []
+    
+    for sentence in tqdm(text_data):
+        sentence = re.sub(r'[^\w\s]', '', sentence)
+        preprocessed_text.append(' '.join(token.lower()
+                                  for token in str(sentence).split()
+                                  if token not in stopwords.words('english')))
+
+    return preprocessed_text
+  
+preprocessed_review = preprocess_text(data['Title'].values)
+data['Title'] = preprocessed_review
